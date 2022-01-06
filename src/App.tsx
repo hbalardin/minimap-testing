74f6d49f14d150ea55078b6419ab60bd4c9bb736
Henrique Balardin <balardinh@gmail.com>
2022-01-06 12:59:14 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const MINIMAP_DIMENSIONS = { width: 160, height: 220 };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      className="App"
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <div
+        id="minimap"
+        style={{
+          width: MINIMAP_DIMENSIONS.width,
+          height: MINIMAP_DIMENSIONS.height,
+          background: '#a00',
+          position: 'relative',
+          // overflow: 'hidden',
+        }}
+      >
+        <div
+          id="wrapper-grid"
+          style={{
+            position: 'absolute',
+            top: -MINIMAP_DIMENSIONS.height,
+            left: -MINIMAP_DIMENSIONS.width,
+
+            display: 'grid',
+            gridTemplateColumns: `${MINIMAP_DIMENSIONS.width}px 150px ${MINIMAP_DIMENSIONS.width}px`,
+            gridTemplateRows: `${MINIMAP_DIMENSIONS.height}px 80px ${MINIMAP_DIMENSIONS.height}px`,
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <div id="wrapper" style={{ background: '#0005' }}></div>
+          <div id="wrapper" style={{ background: '#0005' }}></div>
+          <div id="wrapper" style={{ background: '#0005' }}></div>
+          <div id="wrapper" style={{ background: '#0005' }}></div>
+          <div id="wrapper" style={{ background: 'transparent' }}></div>
+          <div id="wrapper" style={{ background: '#0005' }}></div>
+          <div id="wrapper" style={{ background: '#0005' }}></div>
+          <div id="wrapper" style={{ background: '#0005' }}></div>
+          <div id="wrapper" style={{ background: '#0005' }}></div>
+        </div>
+      </div>
     </div>
   );
 }
